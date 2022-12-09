437894b6267cc45762bfef9287a9122c2c31b556
sattwik <maitisattwik@gmail.com>
2022-12-09 19:55:36 +0530

    h

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+ //import {AuthProvider}  from "./contexts/AuthProvider.js"
+
+import Main from './components/Main.js'
+import Login from "./components/Login.js"
+import Content from "./components/Content.js"
+
+import Introduction from "./components/Introduction.js"
+import BS from "./components/BS.js"
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+<div style={{ fontFamily: 'Lucida Console, "Courier New", monospace' }}>
+      <Router>
+      
+       <Routes>
+       <Route path="/" element={<Login />} /> 
+         <Route path="/main" element={<Main />} />
+          <Route path='/Content' element={<Content />}/>
+          <Route path='/Introduction' element={<Introduction />}/>
+          <Route path='/BS' element={<BS />}/>
+        
+           </Routes>
+          
+        
+          
+      </Router>
     </div>
-  );
+  
+       
+   ) 
+  
 }
 
-export default App;
+
+export default App
