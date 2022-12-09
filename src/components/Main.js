26aea2f1e7654f0cc31f1906b42a24a3305d3f0d
sattwik <maitisattwik@gmail.com>
2022-12-09 20:45:56 +0530

    H

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import { Router } from 'react-router-dom';
 //const details=JSON.parse(uploaddetails);
 import {Link }from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
-import {pp} from '../ll.png'
+import pp from './ll.png'
 
 const Main = () => {
  // const data=JSON.parse(localStorage.getItem("user"))
@@ -23,7 +23,7 @@ const Main = () => {
    
       <div className="card">
      
-  <img src={pp}  style={{width:"70%",height:'70%'}}/>
+  <img src={pp} style={{width:"70%",height:'70%'}}/>
   
   <h1 style={{paddingBottom:'10px',fontSize:'30px'}}> {data.displayName}</h1>
   <p className="title" style={{fontWeight:"bold" ,color:'black',paddingBottom:'10px',fontSize:'30px'}}  >{data.email}</p>
@@ -31,7 +31,7 @@ const Main = () => {
 
   <p ><button onClick={()=>history('/Content')}>Click to Start Journey</button></p>
   </div>
- 
+
         
     </div>
 </div>
