55ecb712ea019891daff60ff693abd1d16d13573
sattwik <maitisattwik@gmail.com>
2023-01-04 14:38:03 +0530

    g

--- src/components/Login.js
+++ src/components/Login.js
@@ -56,7 +56,7 @@ import { useNavigate } from "react-router-dom"
         <path d="M265.17 125.577l-80-80c-4.88-4.88-12.796-4.88-17.677 0-4.882 4.882-4.882 12.796 0 17.678l58.66 58.66H12.5c-6.903 0-12.5 5.598-12.5 12.5 0 6.903 5.597 12.5 12.5 12.5h213.654l-58.66 58.662c-4.88 4.882-4.88 12.796 0 17.678 2.44 2.44 5.64 3.66 8.84 3.66s6.398-1.22 8.84-3.66l79.997-80c4.883-4.882 4.883-12.796 0-17.678z"/>
       </svg>
     </div>
-    <div className="hello">Sign in with Google</div>
+    <div className="hello" style={{padding:'20px', fontSize:'20px',}}>Sign in with Google</div>
   </div>
   
 </div>
