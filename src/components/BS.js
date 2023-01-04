ae9124ba9e977998066b3d340785739ad0e21951
sattwik <maitisattwik@gmail.com>
2023-01-04 19:09:42 +0530

    g

--- src/components/BS.js
+++ src/components/BS.js
@@ -9,8 +9,8 @@ const BS = () => {
         
        
          
-         <h1 style={{display:'flex',justifyContent:'center'}}>Questions </h1><br />
-         <p >
+         <h1 style={{display:'flex',justifyContent:'center',fontSize:'100px'}}>Questions </h1><br />
+        {/* <p >
          1)<span>Aggresive Cows ::   <a target="_blank"href="https://www.spoj.com/problems/AGGRCOW/" >Question  ----</a> <a target="_blank" href="https://github.com/Sattwikmaiti/ALGORITHMS-GIT-HUB-/blob/main/Searching%20Algorithms/binary_search.cpp">Solution</a></span><br />
          2)<a href="https://codeforces.com/edu/course/2/lesson/6/1/practice" target="_blank">LINK1</a><br />
          3)<a href="https://www.spoj.com/problems/EKO/" target="_blank">LINK2</a><br />
@@ -23,8 +23,266 @@ const BS = () => {
          
                
                </p>
+               */}
 
     </div>
+
+    <table class="table table-hover ">
+  <thead>
+    <tr style={{backgroundColor:'HighlightText',fontSize:'40px'}}>
+      <th scope="col">#</th>
+      <th scope="col">QUESTION NAME</th>
+      <th scope="col">QUESTION LINK</th>
+      <th scope="col">SOLUTION LINK</th>
+    </tr>
+  </thead>
+  <tbody style={{fontSize:'30px'}}>
+    <tr>
+      <th scope="row">1</th>
+      <td style={{color:'black'}}>Aggresive Cows</td>
+      <td> <a target="_blank"href="https://www.spoj.com/problems/AGGRCOW/" >LINK</a></td>
+      <td> <a target="_blank" href="https://github.com/Sattwikmaiti/ALGORITHMS-GIT-HUB-/blob/main/Searching%20Algorithms/binary_search.cpp">Solution</a></td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td style={{color:'black'}}>Aggresive Cows</td>
+      <td> <a target="_blank"href="https://www.spoj.com/problems/AGGRCOW/" >LINK</a></td>
+      <td> <a target="_blank" href="https://github.com/Sattwikmaiti/ALGORITHMS-GIT-HUB-/blob/main/Searching%20Algorithms/binary_search.cpp">Solution</a></td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td style={{color:'black'}}>Aggresive Cows</td>
+      <td> <a target="_blank"href="https://www.spoj.com/problems/AGGRCOW/" >LINK</a></td>
+      <td> <a target="_blank" href="https://github.com/Sattwikmaiti/ALGORITHMS-GIT-HUB-/blob/main/Searching%20Algorithms/binary_search.cpp">Solution</a></td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td style={{color:'black'}}>Aggresive Cows</td>
+      <td> <a target="_blank"href="https://www.spoj.com/problems/AGGRCOW/" >LINK</a></td>
+      <td> <a target="_blank" href="https://github.com/Sattwikmaiti/ALGORITHMS-GIT-HUB-/blob/main/Searching%20Algorithms/binary_search.cpp">Solution</a></td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td style={{color:'black'}}>Aggresive Cows</td>
+      <td> <a target="_blank"href="https://www.spoj.com/problems/AGGRCOW/" >LINK</a></td>
+      <td> <a target="_blank" href="https://github.com/Sattwikmaiti/ALGORITHMS-GIT-HUB-/blob/main/Searching%20Algorithms/binary_search.cpp">Solution</a></td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td style={{color:'black'}}>Aggresive Cows</td>
+      <td> <a target="_blank"href="https://www.spoj.com/problems/AGGRCOW/" >LINK</a></td>
+      <td> <a target="_blank" href="https://github.com/Sattwikmaiti/ALGORITHMS-GIT-HUB-/blob/main/Searching%20Algorithms/binary_search.cpp">Solution</a></td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+     <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr> <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
     </div>
   )
 }
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -2,9 +2,11 @@ import React from 'react'
 import p1 from '../dekh.gif'
 
 import {useNavigate} from 'react-router-dom'
+
 const Introduction = () => {
   const navigate=useNavigate();
   return (
+    <>
    <div>
    <div className="navbar">
        <ul className="all">
@@ -14,9 +16,7 @@ const Introduction = () => {
           <li className="list">
             <a className="link" >Silver  </a>
             <ul className="dropdown">
-              <li className="list">
-                <a className="link" href="#BinarySearch">Binary Search</a>
-              </li>
+             
               <li className="list">
                 <a className="link" href="#Sorting">Sorting Techniques</a>
               </li>
@@ -27,7 +27,7 @@ const Introduction = () => {
                 <a className="link" href="#BitManupulation">Bit Manupulation</a>
               </li>
               <li className="list">
-                <a className="link" href="#Graphs">Graphs</a>
+                <a className="link" href="#Arrays">Arrays</a>
               </li>
             
             </ul>
@@ -36,18 +36,21 @@ const Introduction = () => {
           
             <a className="link" >Gold </a>
             <ul className="dropdown">
+            <li className="list">
+                <a className="link" href="#NumberTheory">NumberTheory</a>
+              </li>
              
               <li className="list">
-                <a className="link" href="#Graphs">Graphs</a>
+                <a className="link" href="#LinkedList">Linked List</a>
               </li>
               <li className="list">
-                <a className="link" href="#Array">Arrays</a>
+                <a className="link" href="#BinarySearch">Binary Search</a>
               </li>
               <li className="list">
-                <a className="link" href="#Recursion">Recursion</a>
+                <a className="link" href="#Heaps">Heaps</a>
               </li>
               <li className="list">
-                <a className="link" href="#Recursion">Recursion</a>
+                <a className="link" href="#StacksQueue">Stacks Queue</a>
               </li>
             </ul>
           </li>
@@ -56,17 +59,12 @@ const Introduction = () => {
             <a className="link" >Diamond </a>
             <ul className="dropdown">
              
+             
               <li className="list">
-                <a className="link" href="#Graphs">Graphs</a>
+                <a className="link" href="#Trees">Trees</a>
               </li>
               <li className="list">
-                <a className="link" href="#Array">Arrays</a>
-              </li>
-              <li className="list">
-                <a className="link" href="#Recursion">Recursion</a>
-              </li>
-              <li className="list">
-                <a className="link" href="#Recursion">Recursion</a>
+                <a className="link" href="#Backtracking">Backtracking</a>
               </li>
             </ul>
           </li>
@@ -76,17 +74,13 @@ const Introduction = () => {
             <ul className="dropdown">
              
               <li className="list">
+                <a className="link" href="#DynamicProgramming">DynamicPrograming</a>
+              </li>
+              <li className="list">
                 <a className="link" href="#Graphs">Graphs</a>
               </li>
-              <li className="list">
-                <a className="link" href="#Array">Arrays</a>
-              </li>
-              <li className="list">
-                <a className="link" href="#Recursion">Recursion</a>
-              </li>
-              <li className="list">
-                <a className="link" href="#Recursion">Recursion</a>
-              </li>
+             
+              
             </ul>
           </li>
         </ul>
@@ -94,11 +88,13 @@ const Introduction = () => {
       <div id="BinarySearch" className="container" >
            <div className="product-details">
             <p className="information" >
-            It is a searching technique used to search an element(say target) in a sorted(ascending or descending) list of elements.In this approach we use two pointers , one for the low_index and another for the high_index, i.e low_index=0 and high_index= n-1 (here n is the length of the sorted list )
+              <h1> Binary Search</h1>
+              Binary search works on sorted arrays. Binary search begins by comparing an element in the middle of the array with the target value. If the target value matches the element, its position in the array is returned. If the target value is less than the element, the search continues in the lower half of the array. If the target value is greater than the element, the search continues in the upper half of the array. By doing this, the algorithm eliminates the half in which the target value cannot lie in each iteration
          
 
             </p>
-            <div className="btn from-left" onClick={()=>navigate('/BS')} >Details</div>
+            < div style={{paddingLeft :'70px'}}>
+            <div className="btn from-left" onClick={()=>navigate('/BS')} >QuestionBank</div></div>
            </div>
 
            <div className="product-image">
@@ -199,6 +195,7 @@ const Introduction = () => {
 
 
    </div>
+   </>
   )
 }
 
