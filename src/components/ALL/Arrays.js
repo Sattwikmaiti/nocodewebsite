2467160b09983f83bdbcc841b586b6cdbcdac68d
sattwik <90182231+Sattwikmaiti@users.noreply.github.com>
2023-01-19 23:21:54 +0530

    Update Arrays.js

--- src/components/ALL/Arrays.js
+++ src/components/ALL/Arrays.js
@@ -10,20 +10,7 @@ const Arrays = () => {
        
          
          <h1 style={{display:'flex',justifyContent:'center',fontSize:'100px'}}>Questions </h1><br />
-        {/* <p >
-         1)<span>Aggresive Cows ::   <a target="_blank"href="https://www.spoj.com/problems/AGGRCOW/" >Question  ----</a> <a target="_blank" href="https://github.com/Sattwikmaiti/ALGORITHMS-GIT-HUB-/blob/main/Searching%20Algorithms/binary_search.cpp">Solution</a></span><br />
-         2)<a href="https://codeforces.com/edu/course/2/lesson/6/1/practice" target="_blank">LINK1</a><br />
-         3)<a href="https://www.spoj.com/problems/EKO/" target="_blank">LINK2</a><br />
-        
-         4)<a target="_blank"href="https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb0f5/0000000000ba84ae">LINK4</a><br />
-         5)<a target="_blank"href="https://www.geeksforgeeks.org/painters-partition-problem/">LINK5</a><br />
-         6)<a target="_blank" href="https://practice.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article">LINK6</a><br />
-         7)<a target="_blank"href="https://www.hackerrank.com/contests/recode-24-0/challenges/light-and-primes/problem">LINK7</a><br />
-         8)<a target="_blank"href="https://practice.geeksforgeeks.org/problems/find-nth-root-of-m5843/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article">LINK8</a><br />
-         
-               
-               </p>
-               */}
+      
 
     </div>
  <div style={{padding :'50px'}}>
