f633ef658fa3db1b255c4bbec94e4181a814f9c5
sattwik <maitisattwik@gmail.com>
2023-01-19 22:20:18 +0530

    g

--- src/components/Contribution.js
+++ src/components/Contribution.js
@@ -4,14 +4,16 @@ import p2 from './t2.gif'
 import emailjs from '@emailjs/browser';
 import { useState } from 'react';
 import './Login.css'
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import {useNavigate} from 'react-router-dom'
-  
+  import Thanks from "./Thanks.js"
 const Contribution = () => {
   const nav=useNavigate();
     const [name, setname] = useState('');
     const [emailid, setemailid] = useState('');
     const [message, setmessage] = useState('');
-   
+    const [isopen, setisopen] = useState(true);
    
 
     
@@ -24,8 +26,9 @@ const Contribution = () => {
     setmessage('');
     setemailid('');
     setname('');
+    setisopen(false)
 
-      alert("THANKS FOR THE REVIEW ")
+      
     
         
  /* emailjs.send( 'service_mw422g6', 'template_thnxeur', {
@@ -54,164 +57,226 @@ const Contribution = () => {
   return (
     
 
+<>
 
-    <>
+
+
+{isopen && 
+
+
+(
+<>
 
     
-    <div className="container" id="gg">
-      <div className="pay">
+<div className="container" id="gg">
+  <div className="pay">
 
-          IF YOU LIKED OUR CONTENT , YOU MAY DONATE RUPEE 1 TO THIS BARCODE . IT WOULD MOTIVATE US 
-      </div>
-       <div className="body">
-        <div className="imgg">
-       <img src={p1}  /></div>
-       <div className="forms">
-       <form >
+      IF YOU LIKED OUR CONTENT , YOU MAY DONATE RUPEE 1 TO THIS BARCODE . IT WOULD MOTIVATE US 
+  </div>
+   <div className="body">
+    <div className="imgg">
+   <img src={p1}  /></div>
+   <div className="forms">
+   <form >
 
-       <div className="txt">
-      <input
-        type="text"
-        id="name"
-        name="name"
-        value={name}
-        placeholder="NAME"
-        onChange={(event) =>
-          setname(event.target.value)
-        }
-      
-      />
+   <div className="txt">
+  <input
+    type="text"
+    id="name"
+    name="name"
+    value={name}
+    placeholder="NAME"
+    onChange={(event) =>
+      setname(event.target.value)
+    }
+  
+  />
 </div>
-     
+ 
 <div className="txt">
-      <input
-        type="email"
-        id="emailid"
-        name="emailid"
-        value={emailid}
-        placeholder="EMAIL"
-        onChange={(event) => {
-          setemailid(event.target.value);
-        }}
-         
-      />
-      </div>
-     <div className="txt">
+  <input
+    type="email"
+    id="emailid"
+    name="emailid"
+    value={emailid}
+    placeholder="EMAIL"
+    onChange={(event) => {
+      setemailid(event.target.value);
+    }}
+     
+  />
+  </div>
+ <div className="txt">
 
 
 <textarea
-        type="text"
-        id="message"
-        rows="4" cols="50"
-        name="message"
-        value={message}
-        placeholder="LEAVE A MESSAGE FOR US"
-        
-       style={{opacity:'1',color:'black',fontSize:'14px'}}
-        onChange={(event) => {
-          setmessage(event.target.value);
-        }}
-        
-      />
-</div>
-      
-         <div className="txt">
-      <button type="submit" onClick={sendEmail
-    }>Submit</button>
-</div>
-     
-      
-    </form>
-    </div>
-    </div>
-    <h1 style={{display:'flex',justifyContent:'center',fontSize:'30px',padding:'10px',fontFamily:'c',color:'black'}}>SPECIAL THANKS TO CONTRIBUTORS </h1>
-    </div>
+    type="text"
+    id="message"
+    rows="4" cols="50"
+    name="message"
+    value={message}
+    placeholder="LEAVE A MESSAGE FOR US"
     
-    <div class="container">
-       
-         <div className="elements">
-         <div className="d">
-	<div class="avatar">
-		<a href="https://codepen.io/MarioDesigns/" target="_blank">
-			<img src={p2}alt="Skytsunami" />
-		</a>
-	</div>
-  </div>   <div className="d">
-	<div class="avatar">
-		<a href="https://codepen.io/MarioDesigns/" target="_blank">
-			<img src={p2}alt="Skytsunami" />
-		</a>
-	</div>
-  </div>   <div className="d">
-	<div class="avatar">
-		<a href="https://codepen.io/MarioDesigns/" target="_blank">
-			<img src={p2}alt="Skytsunami" />
-		</a>
-	</div>
-  </div>   <div className="d">
-	<div class="avatar">
-		<a href="https://codepen.io/MarioDesigns/" target="_blank">
-			<img src={p2}alt="Skytsunami" />
-		</a>
-	</div>
-  </div>
-          <div className="d">
-	<div class="avatar">
-		<a href="https://codepen.io/MarioDesigns/" target="_blank">
-			<img src={p2}alt="Skytsunami" />
-		</a>
-	</div>
-  </div>
-
-  <div className="d">
-	<div class="avatar">
-		<a href="https://codepen.io/MarioDesigns/" target="_blank">
-			<img src={p2}alt="Skytsunami" />
-		</a>
-	</div>
-  </div>
+   style={{opacity:'1',color:'black',fontSize:'14px'}}
+    onChange={(event) => {
+      setmessage(event.target.value);
+    }}
+    
+  />
+</div>
   
+     <div className="txt">
+  <button type="submit" onClick={sendEmail
+}>Submit</button>
+</div>
+ 
+  
+</form>
+</div>
+</div>
+<h1 style={{display:'flex',justifyContent:'center',fontSize:'30px',padding:'10px',fontFamily:'c',color:'black'}}>SPECIAL THANKS TO CONTRIBUTORS </h1>
+</div>
+
+<div class="container">
+   
+     <div className="elements">
+     <div className="d">
+<div class="avatar">
+<a href="https://codepen.io/MarioDesigns/" target="_blank">
+  <img src={p2}alt="Skytsunami" />
+</a>
+</div>
+</div>   <div className="d">
+<div class="avatar">
+<a href="https://codepen.io/MarioDesigns/" target="_blank">
+  <img src={p2}alt="Skytsunami" />
+</a>
+</div>
+</div>   <div className="d">
+<div class="avatar">
+<a href="https://codepen.io/MarioDesigns/" target="_blank">
+  <img src={p2}alt="Skytsunami" />
+</a>
+</div>
+</div>   <div className="d">
+<div class="avatar">
+<a href="https://codepen.io/MarioDesigns/" target="_blank">
+  <img src={p2}alt="Skytsunami" />
+</a>
+</div>
+</div>
+      <div className="d">
+<div class="avatar">
+<a href="https://codepen.io/MarioDesigns/" target="_blank">
+  <img src={p2}alt="Skytsunami" />
+</a>
+</div>
+</div>
+
+<div className="d">
+<div class="avatar">
+<a href="https://codepen.io/MarioDesigns/" target="_blank">
+  <img src={p2}alt="Skytsunami" />
+</a>
+</div>
+</div>
+
 
 
 </div>
 
 <div className="elements">
-         <div className="d">
-	<div class="avatar">
-		<a href="https://codepen.io/MarioDesigns/" target="_blank">
-			<img src={p2}alt="Skytsunami" />
-		</a>
-	</div>
-  </div>   <div className="d">
-	<div class="avatar">
-		<a href="https://codepen.io/MarioDesigns/" target="_blank">
-			<img src={p2}alt="Skytsunami" />
-		</a>
-	</div>
-  </div>   <div className="d">
-	<div class="avatar">
-		<a href="https://codepen.io/MarioDesigns/" target="_blank">
-			<img src={p2}alt="Skytsunami" />
-		</a>
-	</div>
-  </div>   <div className="d">
-	<div class="avatar">
-		<a href="https://codepen.io/MarioDesigns/" target="_blank">
-			<img src={p2}alt="Skytsunami" />
-		</a>
-	</div>
-  </div>
-        
+     <div className="d">
+<div class="avatar">
+<a href="https://codepen.io/MarioDesigns/" target="_blank">
+  <img src={p2}alt="Skytsunami" />
+</a>
+</div>
+</div>   <div className="d">
+<div class="avatar">
+<a href="https://codepen.io/MarioDesigns/" target="_blank">
+  <img src={p2}alt="Skytsunami" />
+</a>
+</div>
+</div>   <div className="d">
+<div class="avatar">
+<a href="https://codepen.io/MarioDesigns/" target="_blank">
+  <img src={p2}alt="Skytsunami" />
+</a>
+</div>
+</div>   <div className="d">
+<div class="avatar">
+<a href="https://codepen.io/MarioDesigns/" target="_blank">
+  <img src={p2}alt="Skytsunami" />
+</a>
+</div>
+</div>
+    
 
- 
+
+
+
+
+</div>
+</div>
+
+
+</>
+
+
+
+)
+
+
+
+
+
+
+
+
+
+
+
+}
+{!isopen &&  
+(
+   <div className="worddd">
+         <div class="words word-1">
+  <span>N</span>
+  <span>O</span>
+  <span>C</span>
+  <span>O</span>
+  <span>D</span>
+  <span>E</span>
   
-
-
-</div>
 </div>
 
+<div class="words word-2">
+  <span>I</span>
+  <span>S</span>
+</div>
 
-    </>
+<div class="words word-3">
+  <span>C</span>
+  <span>O</span>
+  <span>O</span>
+  <span>L</span>
+</div>
+
+
+
+
+
+         <button type="submit" onClick={()=>setisopen(true)
+    }>BACK</button>
+
+   </div>
+)
+
+
+
+}
+</>
   )
       }
 
--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -1,13 +1,134 @@
 import React from 'react'
-import p1 from './t1.gif'
+import p1 from './123.jpeg' 
 import p2 from './t2.gif'
-import p3 from './t3.gif'
+import emailjs from '@emailjs/browser';
+import { useState } from 'react';
+import './Login.css'
 import "./thanks.css"
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import {useNavigate} from 'react-router-dom'
 const Thanks = () => {
+	const nav=useNavigate();
+    const [name, setname] = useState('');
+    const [emailid, setemailid] = useState('');
+    const [message, setmessage] = useState('');
+   
+   
+
+    
+    const sendEmail=(e)=> {
+      e.preventDefault();
+   
+    console.log(name);
+    console.log(emailid);
+    console.log(message)
+    setmessage('');
+    setemailid('');
+    setname('');
+
+	
+      alert("THANKS FOR THE REVIEW ")
+    
+        
+ /* emailjs.send( 'service_mw422g6', 'template_thnxeur', {
+         
+          from_name: name,
+          message: message,
+           reply_to :emailid,
+             
+    
+    
+      }, 'UHku05c1fsztWeJut')
+        .then((result) => {
+    
+            console.log(result.text);
+            setname('');
+      setemailid('');
+      setmessage('');
+      nav('/thank')
+        }, (error) => {
+            console.log(error.text);
+        }); 
+    */
+      }
+     
   return (
-    <div>
-       <div class="container">
-        <h1 style={{display:'flex',justifyContent:'center',fontSize:'30px',padding:'10px',fontFamily:'c',color:'black'}}>SPECIAL THANKS TO CONTRIBUTORS </h1>
+    
+    <>
+
+    
+    <div className="container" id="gg">
+      <div className="pay">
+
+          IF YOU LIKED OUR CONTENT , YOU MAY DONATE RUPEE 1 TO THIS BARCODE . IT WOULD MOTIVATE US 
+      </div>
+       <div className="body">
+        <div className="imgg">
+       <img src={p1}  /></div>
+       <div className="forms">
+       <form >
+
+       <div className="txt">
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={name}
+        placeholder="NAME"
+        onChange={(event) =>
+          setname(event.target.value)
+        }
+      
+      />
+</div>
+     
+<div className="txt">
+      <input
+        type="email"
+        id="emailid"
+        name="emailid"
+        value={emailid}
+        placeholder="EMAIL"
+        onChange={(event) => {
+          setemailid(event.target.value);
+        }}
+         
+      />
+      </div>
+     <div className="txt">
+
+
+<textarea
+        type="text"
+        id="message"
+        rows="4" cols="50"
+        name="message"
+        value={message}
+        placeholder="LEAVE A MESSAGE FOR US"
+        
+       style={{opacity:'1',color:'black',fontSize:'14px'}}
+        onChange={(event) => {
+          setmessage(event.target.value);
+        }}
+        
+      />
+</div>
+      
+         <div className="txt">
+      <button type="submit" onClick={sendEmail
+    }>Submit</button>
+</div>
+     
+      
+    </form>
+    </div>
+    </div>
+    <h1 style={{display:'flex',justifyContent:'center',fontSize:'30px',padding:'10px',fontFamily:'c',color:'black'}}>SPECIAL THANKS TO CONTRIBUTORS </h1>
+    </div>
+    
+    <div class="container">
+       
          <div className="elements">
          <div className="d">
 	<div class="avatar">
@@ -90,8 +211,7 @@ const Thanks = () => {
 </div>
 
 
-
-    </div>
+    </>
   )
 }
 
